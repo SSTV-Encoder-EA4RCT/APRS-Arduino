c2bef27042a899502c1c677cb6a24d2c7ddc7087
Fran Acien <amil101ftw@gmail.com>
2020-04-22 20:13:29 +0200

    read GPS

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,8 @@ typedef struct {
   bool fix;
   float lat;
   float lon;
+  char latd;
+  char lond;
 } Gps;
 
 // Constants
@@ -144,10 +146,11 @@ char rmc[100];
 char rmc_stat;
 
 // Function prototypes
-void timer2_interrupt(void);
+//void timer2_interrupt(void);
 void read_mad(vector<int16_t>* m);
 void read_imu(vector<int16_t>* a, vector<int16_t>* g);
 void read_bar(Bar* res);
+void read_gps(Gps* res);
 void print_altimu(void);  // Print values of mad imu and bar
 void tone(uint32_t ulPin, uint32_t frequency, int32_t duration);  //Tone using buzzer
 void noTone(uint32_t ulPin);    // Tone using Buzzer
@@ -240,58 +243,27 @@ void loop() {
   randomize(tx_delay, 5000, 9000);
 }
 
-void timer2_interrupt(){
-  // read data from the GPS in the 'main loop'
+void read_gps(Gps* res){
   char c = GPS.read();
   // if you want to debug, this is a good time to do it!
   if (GPSECHO)
   if (c) Serial.print(c);
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
     return; // we can fail to parse a sentence in which case we should just wait for another
   }
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis()) timer = millis();
 
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-    Serial.print("\nTime: ");
-    if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    if (GPS.milliseconds < 10) {
-      Serial.print("00");
-    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-      Serial.print("0");
-    }
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-    }
+  res->fix = GPS.fix;
+
+  if (GPS.fix) {
+    res->lat = GPS.latitudeDegrees;
+    res->lon = GPS.longitudeDegrees
+    res->latd = GPS.lat;
+    res->lond = GPS.lon;
   }
 }
+
 
 void read_mad(vector<int16_t>* m){
   mag.read();
